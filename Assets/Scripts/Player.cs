c9691b018caf725cb0df70c6e2eee443c41d4365
ShlepShlep <agne.sileviciute@gmail.com>
2024-01-07 14:11:08 +0200

    Visuals

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject cherry;
+    public GameObject strawberry;
+    public GameObject grape;
+    public GameObject gyool;
+    public GameObject orange;
+    public GameObject apple;
+    public GameObject pear;
+    public GameObject peach;
+    public GameObject pineapple;
+    public GameObject melon;
+    public GameObject watermelon;
+
+    public List<GameObject> startingFruits = new List<GameObject> { cherry, strawberry };
+
+    void Start()
+    {
+        
+    }
+
+    public void GetRandomFruit()
+    {
+
+    }
+
+}
