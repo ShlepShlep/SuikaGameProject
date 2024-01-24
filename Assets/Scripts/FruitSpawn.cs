8cb65284b122c17cef85f996eb74b97bb5f94b1e
Shlep <agne.sileviciute@gmail.com>
2024-01-24 17:34:30 +0200

    sdagasdgfinish

--- Assets/Scripts/FruitSpawn.cs
+++ Assets/Scripts/FruitSpawn.cs
@@ -1,11 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Video;
+using Random = UnityEngine.Random;
+using UnityEngine.SceneManagement;
 
 public class FruitSpawn : MonoBehaviour
 {
     public bool inCloud = true;
+    private bool timeToCheck = false;
+    
+    //static public int rounded;
+    
     void Start()
     {
         if(transform.position.y < 2.7)
@@ -13,6 +21,7 @@ public class FruitSpawn : MonoBehaviour
             inCloud = false;
             GetComponent<Rigidbody2D>().gravityScale = 0.5f;
         }
+        
     }
 
     // Update is called once per frame
@@ -20,14 +29,16 @@ public class FruitSpawn : MonoBehaviour
     {
         if (inCloud)
         {
+            
             GetComponent<Transform>().position = Player.fruitSpawnPosition.position;
         }
 
         if (Input.GetMouseButtonUp(0))
         {
+            
             GetComponent<Rigidbody2D>().gravityScale = 0.5f;
             inCloud = false;
-            Player.isSpawned = false;
+            StartCoroutine(checkGameOver());
             
         }
     }
@@ -36,10 +47,84 @@ public class FruitSpawn : MonoBehaviour
     {
         if(collision.gameObject.tag == gameObject.tag)
         {
+            #region score
+            if (collision.gameObject.tag == "1")
+            {
+                Player.score += 1;
+            }
+            else if(collision.gameObject.tag == "2") 
+            { 
+                Player.score += 3; 
+            }
+            else if (collision.gameObject.tag == "3")
+            {
+                Player.score += 6;
+            }
+            else if (collision.gameObject.tag == "4")
+            {
+                Player.score += 10;
+            }
+            else if (collision.gameObject.tag == "5")
+            {
+                Player.score += 15;
+            }
+            else if (collision.gameObject.tag == "6")
+            {
+                Player.score += 21;
+            }
+            else if (collision.gameObject.tag == "7")
+            {
+                Player.score += 28;
+            }
+            else if (collision.gameObject.tag == "8")
+            {
+                Player.score += 36;
+            }
+            else if (collision.gameObject.tag == "9")
+            {
+                Player.score += 45;
+            }
+            else if (collision.gameObject.tag == "10")
+            {
+                Player.score += 55;
+            }
+            else if (collision.gameObject.tag == "11")
+            {
+                Player.score += 66;
+            }
+            #endregion
             Player.newFruitSpawnPos = transform.position;
             Player.newFruit = true;
             Player.whichFruit = int.Parse(gameObject.tag);
+            /*score = score * Random.Range(1.01f, 1.05f); ;
+            rounded = (int)Math.Ceiling(score);*/
+            PlayerPrefs.SetInt("score", Player.score);
             Destroy(gameObject);
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("GameOver") && timeToCheck == true)
+        {
+            if(PlayerPrefs.GetInt("score")<= Player.score)
+            {
+                PlayerPrefs.SetInt("score", Player.score);
+            }
+            Debug.Log("Game over");
+            SceneManager.LoadScene("EndScreen");
+            
+        }
+    }
+
+    IEnumerator checkGameOver()
+    {
+        yield return new WaitForSeconds(2f);
+        timeToCheck = true;
+    }
+
+    public void Score()
+    {
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,27 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.Video;
 
 public class Player : MonoBehaviour
 {
     public Transform[] fruitArr;
-    static public bool isSpawned = false;
+    static public Transform nextFruit;
+    public Transform currentFruit;
+    static public string isSpawned = "n";
     static public Vector2 playerPosX;
 
     public Transform fruitSpawnPositionNOTSTATIC;
     static public Transform fruitSpawnPosition;
+    public Transform nextFruitSpawnPosition;
 
     static public Vector2 newFruitSpawnPos;
     static public bool newFruit = false;
 
     static public int whichFruit = 1;
+    static public int score = 0;
+
+    public TMPro.TextMeshPro scoreText;
+    public TMPro.TextMeshPro bestScoreText;
 
     void Start()
     {
         Vector3 mousePosition = Input.mousePosition;
         fruitSpawnPosition = fruitSpawnPositionNOTSTATIC;
-        GetComponent<ParticleSystem>();
+        bestScoreText.text = PlayerPrefs.GetInt("score").ToString();
+        score = 0;
+        //scoreText.text = "0";
+        //currentFruit = fruitArr[Random.Range(0, 4)];
+        //nextFruit = fruitArr[Random.Range(0, 4)];
     }
 
     private void Update()
@@ -35,18 +47,28 @@ public class Player : MonoBehaviour
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(mousePosition.x, transform.position.y, transform.position.z);
 
-        if (mousePosition.x < -1.87) transform.position = new Vector3(-1.87f, transform.position.y, transform.position.z);
-        if (mousePosition.x > 3) transform.position = new Vector3(3f, transform.position.y, transform.position.z);
+        if (mousePosition.x < -1.2f) transform.position = new Vector3(-1.2f, transform.position.y, transform.position.z);
+        if (mousePosition.x > 3.3f) transform.position = new Vector3(3.3f, transform.position.y, transform.position.z);
+
+        if (Input.GetMouseButtonUp(0) && (isSpawned == "y"))
+        {
+            isSpawned = "n";
+
+            currentFruit = nextFruit;
+        }
+
+        scoreText.text = score.ToString();
     }
 
     private void SpawnFruit()
     {
-        if (isSpawned == false)
+        if (isSpawned == "n")
         {
-            StartCoroutine(Timer());
-
             
-            isSpawned = true;
+            nextFruit = fruitArr[Random.Range(0, 4)];
+            StartCoroutine(Timer());
+            isSpawned = "w";
+            
         }
     }
 
@@ -63,6 +85,11 @@ public class Player : MonoBehaviour
     {
         yield return new WaitForSeconds(0.75f);
         Instantiate(fruitArr[Random.Range(0, 4)], fruitSpawnPosition.position, fruitArr[0].rotation);
+        //Instantiate(nextFruit, nextFruitSpawnPosition.position, fruitArr[0].rotation);
+        //nextFruit.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        //Destroy(nextFruit.gameObject);
+        //DestroyImmediate(nextFruit.gameObject, true);
+        isSpawned = "y";
     }
 
 }
